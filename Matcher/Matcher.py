5ef6763ce9da0d53fa88ca4a434e2965a146fc06
Joey Teng <joey.teng.dev@gmail.com>
2015-07-30 14:03:34 +0800

    Matcher.py:\n Partly Completed

--- Matcher/Matcher.py
+++ Matcher/Matcher.py
@@ -15,7 +15,54 @@ def GetInput(TransferFunction, String):
         except ValueError:
             pass
 
+def Reading(path):
+    Graph = {}
+    input = open(path, 'rb')
+#    level = {}
+
+    for line in input:
+        Point = {}
+        content = line.split(' ')
+        Point['level'] = int(content[1])
+        for point in content[2:]:
+            Point[int(point)] = 1
+        Graph[int(content[0])] = Point
+#        try:
+#            level[int(content[1])].append(int(content[0]))
+#        except KeyError, ErrInfo:
+#            level[ErrInfo[0]] = [int(content[0])]
+        
+    return Graph#, level
+
+def Match(i, j):
+    if len(Graph[i]) == 0:
+        return 1
+
+def Topological_Sorting(Graph):
+    OutDegree = {}
+    Sequence = []
+    for item in Graph.iteritems():
+        if (type(item[0]) == 'int'):
+            OutDegree[item[0]] = len(item[1])
+    while (len(Sequence < len(OutDegree))):
+        Quene = []
+        for point in range(0, len(OutDegree)):
+            if (OutDegree[point] == 0):
+                Quene.append(point)
+        for point in Queue:
+            Sequence.append(point)
+            for item in Graph.iteritems():
+                if (point in item[1]):
+                    OutDegree[item[0]] -= 1
+
+    return Sequence
+
+def PreProcess(Graph):
+    
+    return
+
 def Process():
+
     return
 
 #--main--
